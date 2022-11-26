bef5da2d65e822442d5dcf48096b36fc2a54ffa1
Leonardo Souza <leonardo.vilson@hotmail.com>
2022-11-26 19:38:10 -0300

    feat: create colors theme

--- theme/index.ts
+++ theme/index.ts
@@ -1,8 +1,13 @@
 import { theme, extendTheme } from '@chakra-ui/react'
-import jsonTheme from '../theme.json'
 
 const customTheme = {
-    ...jsonTheme,
+    colors: {
+        purpleTheme: '#5f43b2',
+        blackTheme: '#0e1111',
+        whiteTheme: '#fefdfd',
+        grayTheme: '#b1aebb',
+        grayTheme2: '#3a3153',
+    },
     sizes: {
         ...theme.space,
         max: 'max-content',
@@ -14,6 +19,20 @@ const customTheme = {
             md: '1024px',
             lg: '1280px',
             xl: '1920px'
+        }
+    },
+    styles: {
+        global: {
+            "*": {
+                margin: 0,
+                padding: 0,
+                outline: 0,
+                boxSizing: "border-box"
+            },
+            "html, body, #__next": {
+                height: "100%",
+                fontFamily: "Poppins, sans-serif",
+            }
         }
     }
 }
